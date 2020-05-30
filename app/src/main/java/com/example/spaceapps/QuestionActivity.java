d7bb34bde072f83a0c65ee4ee7a5991862e0dddf
Luan Carlos Klein <luancarlosklein16@gmail.com>
2020-05-30 14:26:27 -0300

    Questoes e respostas aleatorias atraves de listas

--- app/src/main/java/com/example/spaceapps/QuestionActivity.java
+++ app/src/main/java/com/example/spaceapps/QuestionActivity.java
@@ -14,6 +14,9 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import java.util.Random;
+import java.util.Vector;
+
 public class QuestionActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
     private Button buttonX, buttonNext;
     private RadioButton radioButtonA, radioButtonB, radioButtonC, radioButtonD;
@@ -31,6 +34,16 @@ public class QuestionActivity extends AppCompatActivity implements TextToSpeech.
     private MediaPlayer finalEXercises;
 
 
+    ///Season questions
+    private String [] totalQuestion = {"1+1=", "2+2=", "7X10=", "80+2=", "46/23=", "1000+0=", "17-8="};
+    private String [] totalAnswer = {"2", "4", "70", "82", "2", "1000", "9"};
+    private String [] wrongAnswer = {"99", "8", "72%", "1888", "87662", "556"};
+    private RadioButton correctAnswer;
+    private  RadioButton [] options = {radioButtonA, radioButtonB, radioButtonC, radioButtonD};
+    private Vector<Integer> questionsToUse = new Vector<Integer>();
+
+
+
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,15 +53,22 @@ public class QuestionActivity extends AppCompatActivity implements TextToSpeech.
         textoQuestao.setText("questao " + String.valueOf(i) + ":");
 
         question = findViewById(R.id.questionPlace);
-        question.setText("1+1=");
+
 
         buttonX = findViewById(R.id.X);
         buttonNext = findViewById(R.id.buttonNext);
         buttonNext.setText("Enter");
+
         radioButtonA = findViewById(R.id.radioButtonA);
         radioButtonB = findViewById(R.id.radioButtonB);
         radioButtonC = findViewById(R.id.radioButtonC);
         radioButtonD = findViewById(R.id.radioButtonD);
+
+        options[0] =  findViewById(R.id.radioButtonA);
+        options[1] =  findViewById(R.id.radioButtonB);
+        options[2] =  findViewById(R.id.radioButtonC);
+        options[3] =  findViewById(R.id.radioButtonD);
+
         barraProgresso = findViewById(R.id.progressBar);
         barraProgresso.setMax(6);
         barraProgresso.setMin(1);
@@ -59,16 +79,47 @@ public class QuestionActivity extends AppCompatActivity implements TextToSpeech.
         anwserWrong = MediaPlayer.create(this, R.raw.wrong);
         finalEXercises = MediaPlayer.create(this, R.raw.finalwin);
 
+        //Generate questions
+        Random generator = new Random();
+        int n;
+        for (int j = 0; j <= maxQuestions; j++) {
+            n = generator.nextInt(totalQuestion.length);
+            while (questionsToUse.contains(n))
+            {
+                n = generator.nextInt(totalQuestion.length);
+            }
+            questionsToUse.add(n);
+        }
+        question.setText(totalQuestion[questionsToUse.get(0)]);
+
+        Vector<String> answers = new Vector<String>();
+        int posCorrect = generator.nextInt(4);
+        options[posCorrect].setText(totalAnswer[questionsToUse.get(0)]);
+        answers.add(totalAnswer[questionsToUse.get(0)]);
+        correctAnswer = options[posCorrect];
+
+        for (int j = 0; j < 4; j++) {
+            if (j != posCorrect)
+            {
+                n = generator.nextInt(wrongAnswer.length);
+
+                while (answers.contains(wrongAnswer[n])) {
+                    n = generator.nextInt(wrongAnswer.length);
+                }
+                answers.add(wrongAnswer[n]);
+                options[j].setText(wrongAnswer[n]);
+            }
+        }
     }
 
     public void update() {
         aux = findViewById(radioGroup.getCheckedRadioButtonId());
-        if (aux.getId() == radioButtonC.getId()) {
+        if (aux.getId() == correctAnswer.getId()) {
             aux.setBackgroundColor(getResources().getColor(R.color.colorAccent));
             anwserCorrect.start(); // no need to call prepare(); create() does that for you
         } else {
             aux.setBackgroundColor(getResources().getColor(R.color.colorRedLight));
-            radioButtonC.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+            correctAnswer.setBackgroundColor(getResources().getColor(R.color.colorAccent));
             anwserWrong.start(); // no need to call prepare(); create() does that for you
         }
         buttonNext.setText("Next");
@@ -96,8 +147,33 @@ public class QuestionActivity extends AppCompatActivity implements TextToSpeech.
                 finish();
             }else {
                 aux.setBackgroundColor(getResources().getColor(R.color.colorWhite));
-                radioButtonC.setBackgroundColor(getResources().getColor(R.color.colorWhite));
+                correctAnswer.setBackgroundColor(getResources().getColor(R.color.colorWhite));
                 textoQuestao.setText("questao " + String.valueOf(i) + ":");
+
+
+//////////////////////////
+                question.setText(totalQuestion[questionsToUse.get(i-1)]);
+                Random generator = new Random();
+                int n;
+                Vector<String> answers = new Vector<String>();
+                int posCorrect = generator.nextInt(4);
+                options[posCorrect].setText(totalAnswer[questionsToUse.get(i-1)]);
+                answers.add(totalAnswer[questionsToUse.get(i-1)]);
+                correctAnswer = options[posCorrect];
+
+                for (int j = 0; j < 4; j++) {
+                    if (j != posCorrect)
+                    {
+                        n = generator.nextInt(wrongAnswer.length);
+
+                        while (answers.contains(wrongAnswer[n])) {
+                            n = generator.nextInt(wrongAnswer.length);
+                        }
+                        answers.add(wrongAnswer[n]);
+                        options[j].setText(wrongAnswer[n]);
+                    }
+                }
+   /////////////////////////////////                 /////////////////////////////////////
                 barraProgresso.setProgress(i);
                 radioGroup.clearCheck();
                 buttonNext.setText("Enter");
