7f7d8ff01488144474fed519644fcdeee0b8bfc9
Guilherme Gomes <barbozaguilherme717@gmail.com>
2020-05-30 09:18:29 -0300

    progress bar funcionando agr

--- app/src/main/java/com/example/spaceapps/QuestionActivity.java
+++ app/src/main/java/com/example/spaceapps/QuestionActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
@@ -14,6 +15,7 @@ public class QuestionActivity extends AppCompatActivity {
     private RadioButton radioButtonA,radioButtonB,radioButtonC,radioButtonD;
     private TextView textoQuestao;
     private int i = 1,maxQuestions = 5;
+    private ProgressBar barraProgresso;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,18 +28,30 @@ public class QuestionActivity extends AppCompatActivity {
         radioButtonB = findViewById(R.id.radioButtonB);
         radioButtonC = findViewById(R.id.radioButtonC);
         radioButtonD = findViewById(R.id.radioButtonD);
+        barraProgresso = findViewById(R.id.progressBar);
+        barraProgresso.setMax(6);
+        barraProgresso.setMin(1);
+    }
+
+    public void update()
+    {
+        textoQuestao.setText("questao " + String.valueOf(i) + ":");
+        barraProgresso.setProgress(i);
 
     }
 
     public void pressedNext(View view){
+
         if(i == maxQuestions)
         {
+            i++;
+            barraProgresso.setProgress(i);
             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(intent);
             finish();
         }else if (radioButtonA.isChecked() || radioButtonB.isChecked() || radioButtonC.isChecked() || radioButtonD.isChecked()) {
-                    i++;
-                    textoQuestao.setText("questao " + String.valueOf(i) + ":");
+            i++;
+            update();
         }
     }
 
