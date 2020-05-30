7b77edc77787af4054c50e83b3c09884344959a1
Luan Carlos Klein <luancarlosklein16@gmail.com>
2020-05-30 10:23:56 -0300

    Audio

--- app/src/main/java/com/example/spaceapps/QuestionActivity.java
+++ app/src/main/java/com/example/spaceapps/QuestionActivity.java
@@ -1,9 +1,12 @@
 package com.example.spaceapps;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -11,13 +14,16 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
-public class QuestionActivity extends AppCompatActivity {
+public class QuestionActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
     private Button buttonX, buttonNext;
     private RadioButton radioButtonA,radioButtonB,radioButtonC,radioButtonD;
     private TextView textoQuestao;
     private int i = 1,maxQuestions = 5;
     private ProgressBar barraProgresso;
     private RadioGroup radioGroup;
+    private TextToSpeech tts;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +40,9 @@ public class QuestionActivity extends AppCompatActivity {
         barraProgresso.setMax(6);
         barraProgresso.setMin(1);
         radioGroup = findViewById(R.id.radioGroup);
+
+       //Speak sound
+        tts = new TextToSpeech(this, this);
     }
 
     public void update()
@@ -56,6 +65,8 @@ public class QuestionActivity extends AppCompatActivity {
             i++;
             update();
         }
+        //Speak
+        tts.speak("Próxima questão!", tts.QUEUE_FLUSH, null);
     }
 
     public void pressedExit(View view)
@@ -65,4 +76,11 @@ public class QuestionActivity extends AppCompatActivity {
         finish();
     }
 
+    @Override
+    public void onInit(int status) {
+        if(status == TextToSpeech.SUCCESS)
+        {
+
+        }
+    }
 }
