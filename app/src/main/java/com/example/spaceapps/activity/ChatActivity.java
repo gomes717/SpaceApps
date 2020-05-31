be725955623762d71ff54e104da2a27273bd7ba1
Luan Carlos Klein <luancarlosklein16@gmail.com>
2020-05-31 10:32:49 -0300

    Chat Funcionando sem edicao

--- app/src/main/java/com/example/spaceapps/activity/ChatActivity.java
+++ app/src/main/java/com/example/spaceapps/activity/ChatActivity.java
@@ -3,14 +3,34 @@ package com.example.spaceapps.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.spaceapps.R;
 
 public class ChatActivity extends AppCompatActivity {
-
+    private Button buttonSend, buttonRules;
+    private TextView chat;
+    private EditText msg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        buttonSend = findViewById(R.id.buttonSend);
+        msg = findViewById(R.id.msg);
+        chat = findViewById(R.id.chat);
+        chat.setMovementMethod(new ScrollingMovementMethod());
+    }
+
+    public void sendmessage(View view) {
+        String totalChat = chat.getText().toString();
+        String myMessage = msg.getText().toString();
+        totalChat += "\n Ribaldinho: " + myMessage;
+
+        chat.setText(totalChat);
+        msg.setText("");
     }
 }
